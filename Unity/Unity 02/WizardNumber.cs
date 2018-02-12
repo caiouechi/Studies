7008c307afec86679b923e77870ed13d0d0048e9
caiouechi <caio_uechi@hotmail.com>
2018-02-11 23:29:17 -0500

    Add files via upload

---
+++ Unity/Unity 02/WizardNumber.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WizardNumber : MonoBehaviour {
+	int max = 1000;
+	int min = 1;
+	int guess = 500;
+
+	// Use this for initialization
+	void Start () {
+
+	print ("the max number you can pick is " + max);
+	print ("the min number you can pick is " + min);
+	
+	print ("Is the number ?" + guess);
+	
+	print ("Up arrow -> Higher, Down arrow -> lower, return -> return ");
+	}
+	
+	/*
+	o numero eh 500? (1000/2)
+	pra cima -> 500 e 1000 -> perguntar se o numero eh 750
+	pra cima -> 750 e 1000 -> perguntar se eh 875
+	pra cima -> 875 e 1000 -> pergunta se eh 77
+	pra cima -> 938 e 1000 -> pergunta se eh 
+	*/
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+		min = guess;
+		guess = ((min + max)/2);
+		print ("Is the number? " + guess);
+		
+		
+		}else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			max = guess;
+			guess = ((min + max)/2);
+			print ("Is the number? " + guess);
+			
+		}else if(Input.GetKeyDown(KeyCode.Return)) {
+		print ("I won the console");
+		}
+		
+	}
+	
+	
+	
+	
+	
+}
